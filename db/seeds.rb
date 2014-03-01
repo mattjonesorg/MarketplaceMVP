7a3efe6f00456b98f0a8211bdf89f785a391e99a
Matt Jones <git@mattjones.org>
2014-03-01 14:26:40 -0600

    adding some seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Brand.create ([
+		{name: "Starbucks", id: 1},
+		{name: "Target", id: 2},
+		{name: "Home Depot", id: 3},
+		{name: "Menards", id: 4},
+		{name: "Lowes", id: 5},
+		{name: "Jewel", id: 6},
+		{name: "Maggiano's", id: 7},
+		{name: "Chi-Tung", id: 8},
+		{name: "Potbelly", id: 9},
+		{name: "Bed, Bath, and Beyond", id: 10}
+	])
+
+Listing.create([
+		{brand_id: 1, cardnumber: "ABC123", status:"Approved", price: 5.00, value: 10.00 },
+		{brand_id: 2, cardnumber: "123456", status:"Approved", price: 20.00, value: 25.00 },
+		{brand_id: 3, cardnumber: "HD123", status:"Approved", price: 20.00, value: 100.00 },
+		{brand_id: 3, cardnumber: "HD456", status:"Approved", price: 20.00, value: 50.00 },
+		{brand_id: 3, cardnumber: "HD789", status:"Approved", price: 20.00, value: 25.00 },
+		{brand_id: 4, cardnumber: "654321", status:"Approved", price: 20.00, value: 25.00 }
+	])
