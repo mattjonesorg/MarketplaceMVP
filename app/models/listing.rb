23b46049e1397cba1047b93be92c154ea859ca21
Matt Jones <git@mattjones.org>
2014-03-01 00:28:30 -0600

    listings now have some validations on price and value.

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -2,5 +2,6 @@ class Listing < ActiveRecord::Base
   belongs_to :brand
 
   	validates :cardnumber, presence: true, uniqueness:true
-
+  	validates :price, presence: true, numericality: {greater_than_or_equal_to: 0}
+  	validates :value, presence: true, numericality: {greater_than_or_equal_to: 10}
 end
--- spec/models/listing_spec.rb
+++ spec/models/listing_spec.rb
@@ -3,4 +3,8 @@ require 'spec_helper'
 describe Listing do
   it { should validate_presence_of(:cardnumber) }
   it { should validate_uniqueness_of(:cardnumber) }
+  it { should validate_presence_of(:price) }
+  it { should validate_numericality_of(:price).is_greater_than_or_equal_to(0) }
+  it { should validate_presence_of(:value) }
+  it { should validate_numericality_of(:value).is_greater_than_or_equal_to(10) }
 end
